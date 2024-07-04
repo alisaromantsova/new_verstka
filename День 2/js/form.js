c7bac20a7c92f88a5c184497d2903c2a85b48947
alisaromantsova <113140050+alisaromantsova@users.noreply.github.com>
2024-07-04 23:32:06 +0300

    Day 1,2

---
+++ День 2/js/form.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('#subscr-form');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const userNameField = document.querySelector('#user-name');
+    const userEmailField = document.querySelector('#user-email');
+
+    console.log('User name: ', userNameField.value);
+    console.log('User email: ', userEmailField.value);
+});
